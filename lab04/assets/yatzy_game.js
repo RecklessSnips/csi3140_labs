d00c433d5962d95d766b4df3da46aae35e6ac33e
unknown <rliu144@uottwa.ca>
2024-06-07 11:11:00 -0400

    Finish Lab04

--- lab04/assets/yatzy_game.js
+++ lab04/assets/yatzy_game.js
@@ -2,30 +2,45 @@ import { rollDice } from "./dice.js";
 
 let dices = rollDice(5);
 let choice = [];
-console.log(dices);
+// let result = [];
+// console.log(dices);
 
-const selectDice = (...params) => {
+
+const selectDice = (result, ...params) => {
+  console.log(result)
   const dice = params;
-  console.log(dice);
+  console.log('index', dice);
   for (const index of dice) {
-    if (index > dices.length) {
+    if (index > result.length) {
       alert("Too big");
       return;
     }
-    choice.push(dices[index - 1]);
-    dices[index - 1] = 0;
+    choice.push(result[index - 1]);
+    result[index - 1] = 0;
   }
-  let result = dices.filter((choice) => choice !== 0);
+  result = result.filter((choice) => choice !== 0);
   console.log("Choice: ", choice);
-  console.log("Left: ", dices);
-  console.log(result);
+  console.log('Result',result);
+  return result;
 };
 
-selectDice(3, 5, 1);
+// selectDice(3, 5, 1);
 
 const yatzy = () => {
   /*
     Let state be 0: initial roll, 1: second roll, 2: thrid roll
   */
   let state = 0;
+  dices= rollDice(5); 
+  console.log('dices', dices)
+  let result = selectDice(dices, 2,4)
+  state++
+
+  dices = rollDice(result.length)
+  result = selectDice(dices, 1,2)
+
+  dices = rollDice(result.length)
+  selectDice(dices, 1)
 };
+
+yatzy();
